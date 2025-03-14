59b573c8df881a230fb1b0b8315882b7b541a053
Furkan Melih Ercan <furkan.ercan@b2metric.com>
2025-03-14 22:47:51 +0300

    fix: typo fixes

--- circuit_breaker/circuit_breaker.py
+++ circuit_breaker/circuit_breaker.py
@@ -1,6 +1,8 @@
+import traceback
 import pendulum as pe
 from typing import Callable
 from fastapi import Response
+from fastapi.responses import StreamingResponse
 
 from .enums import State
 from .schemas import CircuitBreakerInputDto
@@ -28,10 +30,20 @@ class CircuitBreaker:
     def state(self, state: State):
         self._state = state
 
+    def _check_http_exception(self, response: StreamingResponse):
+        if response.status_code < 400:
+            return
+
+        raise CircuitBreakerRemoteCallException(
+            "Request failed with status code: " + str(response.status_code)
+        )
+
     def update_last_attempt_datetime(self):
         self._last_attempt_datetime = pe.now()
 
     async def handle_open_state(self, func: Callable, *args, **kwargs) -> Response:
+        exception_list = tuple(self.circuit_breaker_input.exception_list)
+
         current_datetime = pe.now()
         threshold_datetime = self._last_attempt_datetime.add(
             seconds=self.circuit_breaker_input.half_open_retry_timeout_seconds
@@ -44,29 +56,34 @@ class CircuitBreaker:
         self.state = State.HALF_OPEN
         try:
             response = await func(*args, **kwargs)
-
+            self._check_http_exception(response)
             self.state = State.CLOSED
             self._failed_count = 0
             self.update_last_attempt_datetime()
             return response
-        except self.circuit_breaker_input.exception_list as e:
+        except exception_list as e:
             self._failed_count += 1
             self.update_last_attempt_datetime()
             self.state = State.OPEN
+
+            traceback.print_exception(e)
             raise CircuitBreakerRemoteCallException
 
     async def handle_closed_state(self, func: Callable, *args, **kwargs) -> Response:
+        exception_list = tuple(self.circuit_breaker_input.exception_list)
         try:
             response = await func(*args, **kwargs)
+            self._check_http_exception(response)
             self.update_last_attempt_datetime()
             return response
-        except self.circuit_breaker_input.exception_list as e:
+        except exception_list as e:
             self._failed_count += 1
             self.update_last_attempt_datetime()
 
             if self._failed_count >= self.circuit_breaker_input.half_open_retry_count:
                 self.state = State.OPEN
 
+            traceback.print_exception(e)
             raise CircuitBreakerRemoteCallException
 
     async def handle_circuit_breaker(self, func: Callable, *args, **kwargs) -> Response:
