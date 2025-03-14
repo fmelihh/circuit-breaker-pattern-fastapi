39b4b123a428158c6b1878bc26868dbb128e043b
Furkan Melih Ercan <furkan.ercan@b2metric.com>
2025-03-14 22:02:14 +0300

    fix: typo fixes

--- circuit_breaker/middleware.py
+++ circuit_breaker/middleware.py
@@ -6,20 +6,15 @@ from .schemas import CircuitBreakerInputDto
 from .circuit_breaker import CircuitBreaker
 
 
-class CircuitBreakerMiddleware(BaseHTTPMiddleware):
+class CircuitBreakerMiddleware(CircuitBreaker, BaseHTTPMiddleware):
     def __init__(
         self, app, circuit_breaker_input: CircuitBreakerInputDto | None = None
     ):
-        super().__init__(app)
-        if circuit_breaker_input is None:
-            circuit_breaker_input = CircuitBreakerInputDto()
-
-        self.circuit_breaker = CircuitBreaker(
-            circuit_breaker_input=circuit_breaker_input
-        )
+        BaseHTTPMiddleware.__init__(self, app)
+        CircuitBreaker.__init__(self, circuit_breaker_input)
 
     async def dispatch(self, request: Request, call_next: Callable):
-        await self.circuit_breaker.handle_circuit_breaker(
+        await self.handle_circuit_breaker(
             func=call_next,
             request=request,
         )
