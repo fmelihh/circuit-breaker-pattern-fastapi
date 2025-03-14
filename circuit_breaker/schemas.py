04205fd7ca0b5de82731bde98e9c05178af1d8be
Furkan Melih Ercan <furkan.ercan@b2metric.com>
2025-03-14 22:13:23 +0300

    fix: typo fixes

--- circuit_breaker/schemas.py
+++ circuit_breaker/schemas.py
@@ -3,7 +3,7 @@ from pydantic import BaseModel, Field, field_validator
 
 class CircuitBreakerInputDto(BaseModel):
     half_open_retry_count: int = Field(default=3, ge=0, le=100)
-    half_open_retry_timeout_seconds = Field(default=120, ge=30, le=1800)
+    half_open_retry_timeout_seconds: int = Field(default=120, ge=30, le=1800)
 
     exception_list: tuple[Exception] = Field(default=(Exception,))
 
